9dcc487e7bef8b134a1170c79a0f4ecba7f8ccb0
ebarruel <barruelevangeline.bchs@gmail.com>
2022-10-08 15:07:14 -0700

    adding stt to button

--- src/components/ChatComposer.jsx
+++ src/components/ChatComposer.jsx
@@ -78,7 +78,11 @@ export default function ChatComposer({ onSend }) {
                     <button type="button" className="sendButtonStyle" onClick={() => {console.log("input:", input); setOnscreenKey(!onscreenKey)}}>
                         <i class="bi bi-keyboard"></i>
                     </button>
-                    <button type="button" className="sendButtonStyle">
+                    <button
+                        type="button"
+                        onClick={handleUserSpeaking}
+                        className="sendButtonStyle"
+                    >
                         <i class="bi bi-mic"></i>
                     </button>
                     <button type="submit" className="sendButtonStyle">
